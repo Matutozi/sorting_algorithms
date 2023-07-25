b6156b2e44b5ea500674e3326e79c779fb01f598
queenoko <quimaudongwo@gmail.com>
2023-07-25 07:42:48 -0700

    header file

--- sort.h
+++ sort.h
@@ -19,12 +19,12 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-void selection_sort(int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap(int *a, int *b);
-void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int lowr, int higr, size_t size);
 void quick_so(int *array, int lowr, int higr, size_t size);
