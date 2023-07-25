8b7d2f3680bf460aa95669e9a1e9b0437bfa66dc
queenoko <quimaudongwo@gmail.com>
2023-07-25 11:09:44 -0700

    merge_sort

--- sort.h
+++ sort.h
@@ -38,5 +38,10 @@ void bitonicsort(int arr[], int lowr, int ele_no, int order, int size);
 void bitonic_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 listint_t *swap_node(listint_t *node, listint_t **list);
+void *_calloc(unsigned int ele_no, unsigned int size);
+void merge(int *arr, int *temp, int start, int mid, int end);
+void mergesort(int *array, int *temp, int start, int end);
+void merge_sort(int *array, size_t size);
+
 
 #endif /*_SORT_H */
